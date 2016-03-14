92ed2acaaa995e38814b39e9e96fe454ab383109
Tilak Patidar <tilakpatidar@gmail.com>
2016-03-14 23:10:16 +0530

    js validation for signup modal

--- static/js/login.js
+++ static/js/login.js
@@ -10,16 +10,76 @@ $("#password").attr('autocomplete','off').val("");
 		  data: {"username":username,"password":password},
 		  success: function(){
 		  		//if we get 200 Response
-		  		alert("Logged in")
+		  		$.toast({
+				    heading: 'Success',
+				    text: 'Login success',
+				    showHideTransition: 'fade',
+				    icon: 'success'
+				});
 
 		  },
 		  error: function(){
 		  		//if we get 404 response
-		  		alert("Login failed")
+		  		$.toast({
+				    heading: 'Error',
+				    text: 'Wrong login credentials',
+				    showHideTransition: 'fade',
+				    icon: 'error'
+				});
 		  }
 		});
 		return false;//stop form submission
-	})
+	});
+
+	$(document).on("click","#user-submit",function(){
+		//for regular user registeration
+		var p1=$("#user-password1").val();
+		var p2=$("#user-password2").val();
+		if((p1!==p2) || (p1==="" && p2==="")){
+			$.toast({
+				    heading: 'Error',
+				    text: 'passwords do not match',
+				    showHideTransition: 'fade',
+				    icon: 'error'
+				});
+			$("#user-password1").val("");
+			$("#user-password2").val("");
+			return;
+		}
+		var name=$("#user-name").val();
+		var email=$("#user-email").val();
+		if(name===""){
+			$.toast({
+				    heading: 'Error',
+				    text: 'Name cannot be left empty',
+				    showHideTransition: 'fade',
+				    icon: 'error'
+				});
+			$("#user-name").val("");
+			return;
+		}
+		if(email===""){
+			$.toast({
+				    heading: 'Error',
+				    text: 'Email cannot be left empty',
+				    showHideTransition: 'fade',
+				    icon: 'error'
+				});
+			$("#user-email").val("");
+			return;
+		}
+		if(email.indexOf("@")<0){
+			$.toast({
+				    heading: 'Error',
+				    text: 'Email type invalid',
+				    showHideTransition: 'fade',
+				    icon: 'error'
+				});
+			$("#user-email").val("");
+			return;
+		}
+		return false;//stop form submission
+	});
 
 
 });
